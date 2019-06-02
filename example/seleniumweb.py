7a43508f00f317abfa33518e83bf2558715e6d70
Anass Safiot <revero275@gmail.com>
2019-06-02 03:07:00 +0200

    added chrome driver path

--- example/seleniumweb.py
+++ example/seleniumweb.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
-driver =webdriver.Chrome("/Users/Anass.Ben/AppData/Local/Programs/Python/Python36/Training/chromedriver")
+driver =webdriver.Chrome("D:/gitrepository/pyvision/webdriver/chromedriver")
 driver.get("https://www.youtube.com/user/noobtoprofessional")
 driver.find_element_by_xpath('//a[contains(text(),"Why you should learn Python Programming")]').click()
